c3ddb118a869840205635a0364975acc106bb9b3
manoel <manoeldiasmf@gmail.com>
2023-10-26 19:59:24 -0300

    fixed total days

--- src/utils/generate-dates-from-year-beginning.ts
+++ src/utils/generate-dates-from-year-beginning.ts
@@ -1,17 +1,16 @@
-import dayjs from "dayjs"
-
+import dayjs from 'dayjs'
 
 export function generateDatesFromYearBeginning() {
   const firstDayOfTheYear = dayjs().startOf('year')
-  
-  const  today = new Date()
+  const today = new Date()
 
   const dates = []
   let compareDate = firstDayOfTheYear
 
   while (compareDate.isBefore(today)) {
     dates.push(compareDate.toDate())
-    compareDate = compareDate.add(1, 'day')
+    compareDate = compareDate.add(30 , 'day')
   }
+
   return dates
 }
